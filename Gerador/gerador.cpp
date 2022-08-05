3cc1893bcf7320ef871c0d946a989b57f50dea81
jeferson01 <jeferson_slip01@hotmail.com>
2022-08-05 18:54:36 -0300

    add baralho.dat

--- Gerador/gerador.cpp
+++ Gerador/gerador.cpp
@@ -7,29 +7,52 @@ using namespace std;
 
 int main() {
 	ifstream fin;
-	fin.open("baralho.dat", ifstream::in);
+	ofstream fout;
+
+	Cartas pikachu = { "Pikachu", 12, 15 };
+	Cartas vetBaralho[32];
+
+	fin.open("../baralho.dat", ifstream::in | ifstream::binary);
 	if (!fin.is_open())
 		cout << "arquivo nao foi aberto.\n";
 
+	const char cabecalho[] = "BARALHO";
+	char chTemp[10];
+	unsigned short quantCartas; // quantidade de cartas no vetor..
 
-	Cartas pikachu = {"Pikachu", 12, 15};
-	Cartas vetBaralho[32];
+	// leitura do cabeçalho
+	fin.read((char*) &chTemp, sizeof(cabecalho));
+	fin.read((char*) &quantCartas, sizeof(short));
+	fin.read((char*) vetBaralho, sizeof(Cartas)* quantCartas); // ler vetor
+	cout << chTemp << " " << quantCartas;
+
+	fin.close();
+
+
 	ExibirMenu();
 
 	char ch;
 	cin >> ch;
-	switch (ch)
+	switch (toupper(ch)) // convert maiusculo
 	{
 	case 'C': 
-		cout << "cadastro";
-		vetBaralho[0] = Cadastrar();
-		cout << vetBaralho[0].nome;
+		//cout << "cadastro";
+		vetBaralho[quantCartas] = Cadastrar();
+		cout << vetBaralho[quantCartas].nome;
+		quantCartas++;
+	
 		
 		break;
 	case 'I': cout << "importar"; break;
 
 	default: cout << "invalido"; break;
 	}
+
+	fout.open("../baralho.dat", ios::out | ios::trunc | ios::binary);
+	//quantCartas = 12; // quantidade de cartas no vetor, definir depois..
+	fout.write((char*) &cabecalho, sizeof(cabecalho));
+	fout.write((char*)&quantCartas, sizeof(short));
+	fout.close();
 
 
 	return 0;
